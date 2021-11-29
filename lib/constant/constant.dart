c30925d582ad47ee14c79e7ea02651fc0f8f926f
Made Raja Adi <47583818+Dema09@users.noreply.github.com>
2021-11-29 10:44:46 +0700

    update constant.dart

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -10,4 +10,4 @@ const fullStackMobileDeveloper = 'Full-Stack Mobile Developer';
 const roadgeek = 'Roadgeek';
 const aboutMeDesc = 'Currently graduated at Bina Nusantara University from School of Computer Science Major. '
     'Experienced on Back-End Development for Web and Mobile App. '
-    'I like problem solving with several programming language, team working, and communicative as well.';;
+    'I like problem solving with several programming language, team working, and communicative as well.';
